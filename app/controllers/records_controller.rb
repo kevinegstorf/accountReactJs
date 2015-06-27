b26f09b7126ba0f776b599caecdda1523d0ec24d
Kevin Egstorf <kevin.egstorf@gmail.com>
2015-06-27 17:32:19 +0200

    finished tutorial

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -13,16 +13,24 @@ class RecordsController < ApplicationController
     end
   end
 
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @record = Record.find(params[:id])
     @record.destroy
     head :no_content
   end
 
-    private
+  private
 
-      def record_params
-        params.require(:record).permit(:title, :amount, :date)
-      end
-
-  end
+    def record_params
+      params.require(:record).permit(:title, :amount, :date)
+    end
+end
